e01960fa3f9d72744d8826ffec1ae2803619bf6f
Michael Fitzgerald <michael.craig.fitzgerald@gmail.com>
2023-09-03 07:42:00 -0400

    campaign calendars

---
+++ ad_campaign_lib.py
@@ -0,0 +1,13 @@
+# Existing Campaigns
+first_three_weeks_per_quarter = [1, 2, 3, 14, 15, 16, 27, 28, 29, 40, 41, 42]
+holiday_season = [48, 49, 50, 51, 52]
+back_to_school = [33, 34, 35]
+summer_sale = [26, 27, 28, 29, 30]
+black_friday = [47]
+
+# Additional Campaigns
+spring_sale = [14, 15, 16, 17, 18]  # Mid-April to Mid-May
+valentines_day = [6, 7]  # Early February
+easter_holiday = [13, 14]  # Early to Mid-April
+halloween = [44]  # End of October
+cyber_monday = [48]  # End of November
